a62c4bb6f9d8e0a75f10269084ef0c86880421e8
WMF <you@example.com>
2025-01-04 18:30:51 +0200

    first commit

---
+++ src/main/java/com/shop/shop/ShopApplication.java
@@ -0,0 +1,15 @@
+package com.shop.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/shop/shop/data/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.shop.shop.data.repositories;
+
+import com.shop.shop.data.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
